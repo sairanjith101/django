9160d0d8d7771ef6d4311ec9411ecaab078183b1
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2023-08-30 17:09:40 +0530

    Delete practice/DBproject directory

--- practice/DBproject/DBapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.Model):
-    studno = models.IntegerField()
-    studname = models.CharField(max_length = 20)
-    studclass = models.IntegerField()
-    studaddress = models.CharField(max_length = 100)
