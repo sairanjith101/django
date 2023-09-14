bbe6458d18aa8362dd62e461f8f56cf7ea9dd577
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2023-09-14 12:20:21 +0530

    Add files via upload

---
+++ practice/cookiesDemo/cookiesApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cookies_count_view(request):
+    count = request.COOKIES.get('count',0)
+    totalCount = int(count) + 1
+    response = render(request, 'cookiesApp/input.html', {'count' : totalCount})
+    response.set_cookie('count', totalCount)
+    return response
